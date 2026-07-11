69aec8a5e9f2a7e1dc517807d419449a3467149d
Vivek kumar <145559400+vivekkr620@users.noreply.github.com>
2026-07-12 00:28:01 +0530

    Time: 0 ms (100%), Space: 44.3 MB (92.8%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -15,25 +15,25 @@ class Solution {
 
     public void nextPermutation(int[] nums) {
         int n = nums.length;
-        int index = -1;
+        int pivot = -1;
         // Step 1: Find the first decreasing element from the right
         for (int i = n - 2; i >= 0; i--) {
             if (nums[i] < nums[i + 1]) {
-                index = i;
+                pivot = i;
                 break;
             }
         }
 
-        // Step 2: Find the next greater element to swap with nums[index]
-        if (index != -1) { // If such an element is found
-            for (int i = n - 1; i > index; i--) {
-                if (nums[i] > nums[index]) {
-                    swap(nums, i, index);
+        // Step 2: Find the next greater element to swap with nums[pivot]
+        if (pivot != -1) { // If such an element is found
+            for (int i = n - 1; i > pivot; i--) {
+                if (nums[i] > nums[pivot]) {
+                    swap(nums, i, pivot);
                     break;
                 }
             }
         }
         // Step 3: Reverse the suffix (right part of the array)
-        reverse(nums, index + 1, n - 1);
+        reverse(nums, pivot + 1, n - 1);
     }
 }
