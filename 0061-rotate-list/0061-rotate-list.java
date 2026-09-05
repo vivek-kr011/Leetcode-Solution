b3c516c9e204a71a45249a130f223ef5a6f59396
Vivek kumar <145559400+vivek-kr011@users.noreply.github.com>
2026-09-06 00:35:15 +0530

    Time: 0 ms (100%), Space: 44.7 MB (10.02%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -10,8 +10,8 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        
-if (head == null || head.next == null || k == 0)
+
+        if (head == null || head.next == null || k == 0)
             return head;
 
         // Initialize length and tail
