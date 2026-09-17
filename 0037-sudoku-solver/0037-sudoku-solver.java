703706822b1bacad0b426b577a8eee1a20599d84
Vivek kumar <145559400+vivek-kr011@users.noreply.github.com>
2026-09-17 23:24:40 +0530

    Time: 180 ms (81.06%), Space: 43 MB (25.3%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,77 +1,76 @@
 class Solution {
 
-    public void solveSudoku(char[][] board) {
-        solve(board, 0, 0);
-    }
-
-    private boolean solve(char[][] board, int row, int col) {
-
-        // Base Case
-        if (row == 9) {
-            return true;
-        }
-
-        // Next Cell
-        int nextRow = row;
-        int nextCol = col + 1;
-
-        if (col == 8) {
-            nextRow = row + 1;
-            nextCol = 0;
-        }
-
-        // Skip filled cells
-        if (board[row][col] != '.') {
-            return solve(board, nextRow, nextCol);
-        }
-
-        // Try digits 1 to 9
-        for (char digit = '1'; digit <= '9'; digit++) {
-
-            if (isSafe(board, row, col, digit)) {
-
-                board[row][col] = digit;
-
-                if (solve(board, nextRow, nextCol)) {
-                    return true;
-                }
-
-                // Backtrack
-                board[row][col] = '.';
-            }
-        }
-
-        return false;
-    }
-
-    private boolean isSafe(char[][] board, int row, int col, char digit) {
-
-        // Check Column
-        for (int i = 0; i < 9; i++) {
-            if (board[i][col] == digit) {
+    private boolean isSafe(char[][] board, int row, int col, int digit) {
+        // horizontal check
+        for(int j = 0; j < 9; j++) {
+            if(board[row][j] == digit) {
                 return false;
             }
         }
 
-        // Check Row
-        for (int j = 0; j < 9; j++) {
-            if (board[row][j] == digit) {
+        // vertically check ( col check )
+        for(int i = 0; i < 9; i++) {
+            if(board[i][col] == digit) {
                 return false;
             }
         }
 
-        // Check 3x3 Grid
-        int sr = (row / 3) * 3;
-        int sc = (col / 3) * 3;
+        // Grid Check (3x3), sr = starting row, sc = starting col O(9)
+        int sr = (row/3) * 3;
+        int sc = (col/3) * 3;
 
-        for (int i = sr; i < sr + 3; i++) {
-            for (int j = sc; j < sc + 3; j++) {
-                if (board[i][j] == digit) {
+        
+
+        for(int i = sr; i < sr + 3; i++) {
+            for(int j = sc; j < sc + 3; j++) {
+                if(board[i][j] == digit) {
                     return false;
                 }
             }
         }
 
         return true;
+
+    }
+
+    private boolean helper(char[][] board, int row, int col) {
+
+        // base case
+        if(row == 9) {
+            return true;
+        }
+
+        // // Calculate next cell coordinates
+        int nextRow = row, nextCol = col + 1;
+        if(nextCol == 9) {
+            nextRow = row + 1;
+            nextCol = 0;
+        }
+
+        // If cell is already filled
+        if(board[row][col] != '.') {
+            return helper(board, nextRow, nextCol);
+        }
+
+        // PLACE THE DIGIT
+        for(char digit = '1'; digit <= '9'; digit++){
+
+            if(isSafe(board, row, col, digit)) {  // => if this function return true
+
+                board[row][col] = digit;  // digit place
+
+                if (helper(board, nextRow, nextCol) ) {// => if this function return true
+                    return true;
+                }
+
+                board[row][col] = '.'; // => replace the digit
+            }
+        }
+
+        return false; // if no ans found
+    }
+
+    public void solveSudoku(char[][] board) {
+        helper(board, 0, 0);
     }
 }
