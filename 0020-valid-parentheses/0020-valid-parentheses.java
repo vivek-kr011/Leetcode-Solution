01e1098a6d3a8ea751d6a8dcc6124336992c751f
Vivek kumar <145559400+vivek-kr011@users.noreply.github.com>
2026-09-24 00:06:07 +0530

    Time: 3 ms (86.14%), Space: 43.1 MB (85.35%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isValid(String str) {
+        
+        Stack<Character> s = new Stack<>();
+
+        for(int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            /* Opening case */
+            if (ch == '(' || ch == '{' || ch == '[') {
+                s.push(ch);
+            } else {
+
+                /* closing case */
+                if (s.isEmpty()) {
+                    return false;
+                }
+
+                if ((s.peek() == '(' && ch == ')')  // () 
+                        || (s.peek() == '{' && ch == '}')  // {}
+                        || (s.peek() == '[' && ch == ']')) {  // []
+
+                    s.pop();
+
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        if (s.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
